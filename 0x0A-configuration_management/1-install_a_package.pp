4b1730fdbfcee3b43cb2b0d75eea19ca5c9bbde4
Carlos Soria <soriac367@gmail.com>
2022-08-14 10:37:58 -0500

    Script that ensures a Python package is installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Script that ensures a Python package is installed
 
-package { 'flask=2.1.0':
-    ensure   => 'installed',
+package { 'flask':
+    ensure   => '2.1.0',
     provider => 'pip3',
 }
