853138e56e6f525c5c3a0686c5df0ef64fd2ae51
Carlos Soria <soriac367@gmail.com>
2022-08-16 19:28:07 -0500

    Puppet manifest to install nginx on ubuntu

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,28 @@
+# Puppet manifest to install nginx on ubuntu
+
+package { 'nginx':
+    ensure   => '1.18.0',
+    provider => 'apt',
+}
+
+exec { 'port 80':
+    command => "ufw allow 'Nginx HTTP'",
+    user    => 'root',
+    path    => '/usr/sbin/ufw',
+}
+
+file { 'index.nginx-debian.html':
+    path    => '/var/www/html/index.nginx-debian.html',
+    content => 'Hellow World',
+}
+
+file_line { 'redirection':
+    path  => '/etc/nginx/sites-available/default',
+    after => 'server_name _;',
+    line  => '\tlocation \/redirect_me {\n\t\treturn 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;\n\t}\n',
+}
+
+exec { 'start service':
+    command => 'service nginx start',
+    path    => '/usr/sbin/service',
+}
